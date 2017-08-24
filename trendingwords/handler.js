2c91680ab582de42605536ed205d78c6a900978d
lisa <lwenke@gmail.com>
2017-08-23 22:14:53 -0700

    set up email notifications

--- trendingwords/handler.js
+++ trendingwords/handler.js
@@ -1,26 +1,90 @@
 'use strict';
 
 const request = require('axios');
-const {extractListingsFromHTML} = require('./helpers');
+const AWS = require('aws-sdk');
+const nodemailer = require('nodemailer');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+const { differenceWith, isEqual } = require('lodash');
+const { extractListingsFromHTML } = require('./helpers');
+const { mailBox } = require('./config');
+
+let url = 'http://www.urbandictionary.com/';
 
 module.exports.gettrendwords = (event, context, callback) => {
-  request('http://www.urbandictionary.com')
+  let newWords, allWords;
+
+  request(url)
   .then(({data}) => {
-  
-    const words = extractListingsFromHTML(data);
-    callback(null, {words})
+
+    allWords = extractListingsFromHTML(data);
+
+
+    //get new
+    return dynamo.scan({
+      TableName: 'trendwords'
+    }).promise();
+
+  })
+  .then(response => {
+    let todayWords = response.Items[0] ? response.Items[0].words : [];
+
+    newWords = differenceWith(allWords, todayWords, isEqual);
+
+    const wordsToDelete = response.Items[0] ? response.Items[0].wordId : null;
+
+    if(wordsToDelete){
+      return dynamo.delete({
+        TableName: 'trendwords',
+        Key: {
+          wordId : wordsToDelete
+        }
+      }).promise();
+
+    }else return;
+
+  })
+  .then(()=>{
+    return dynamo.put({
+      TableName: 'trendwords',
+      Item: {
+        wordId: new Date().toString(),
+        words: allWords
+      }
+    }).promise();
+  })
+  .then(()=>{
+    if (newWords.length) {
+      let transporter = nodemailer.createTransport(mailBox);
+
+      var mailOptions = {
+                  from: '"Trending Slang" <trendingslang@gmail.com>', // sender address
+                  to: 'lwenke@gmail.com',
+                  subject: 'Daily Slang: "' + newWords[0].title+'"', // Subject line
+                  html:
+                  '<a href='+url+newWords[0].link +'><h3> "'+ newWords[0].title +'"</h3></a>'
+                  +'<br>'
+                  +'<p><strong>'+newWords[0].meaning +' </strong> </p>'
+                  +'<br>'
+                  +'<p><em>Example: </em> '+newWords[0].example +'</p>'
+                  +'<br>'
+                  +'<a href='+url+newWords[0].link +'><h5>Link here</h5></a>'
+              };
+
+              transporter.sendMail(mailOptions, function(error, info) {
+                  if (error) {
+                      return console.log(error);
+                  } else {
+                      console.log('emails sent: ', info.response);
+
+
+                  }
+
+              });
+
+
+     }
+    callback(null, {words: newWords})
   })
   .catch(callback);
-  // const response = {
-  //   statusCode: 200,
-  //   body: JSON.stringify({
-  //     message: 'Go Serverless v1.0! Your function executed successfully!',
-  //     input: event,
-  //   }),
-  // };
 
-  // callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
--- trendingwords/helpers.js
+++ trendingwords/helpers.js
@@ -10,26 +10,27 @@ function extractListingsFromHTML (html) {
   const wordSection = $('.def-panel');
   const data_id = $('.def-panel[data-defid]');
 
-  const newWords =[];
+  const newTrends =[];
 
 
 //for each section, scrape new word
   wordSection.each((i, ele)=>{
-    let wordId = $(ele).find('.def-header > .word').attr('name'),
+    let tId = $(ele).find('.def-header > .word').attr('name'),
       trend_date = $(ele).find('.row > .small-6 > .ribbon').first().text().trim(),
       title = $(ele).find('.def-header > .word').first().text().trim(),
       link = $(ele).find('.def-header > .word').attr('href'),
       meaning = $(ele).children('.meaning').first().text().trim(),
-      example = $(ele).children('.example').first().text().trim(),
-      upVotes = $(ele).find('.def-header > .word').attr('href');
+      example = $(ele).children('.example').first().text().trim();
 
-
-    newWords.push({wordId, trend_date, title, link,meaning, example});
+    newTrends.push({tId, trend_date, title, link,meaning, example});
 
   });
-     fs.appendFileSync('words.json', JSON.stringify(newWords) + '\n');
-  return newWords;
+     fs.appendFileSync('words.json', JSON.stringify(newTrends) + '\n');
+  return newTrends;
 }
+
+
+
 module.exports = {
   extractListingsFromHTML
 };
